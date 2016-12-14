2e33d70ce0703738d8244d4fdf3c60a8a91f160f
Tim Walter <twwalter@gmail.com>
2016-12-14 11:18:18 +1100

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
 var React = require('react');
-var TweetBox = require('./TweetBox');
+var _ = require('underscore');
+require('./App.css');
+
 
 var App = React.createClass({
 
   getInitialState: function() {
-    return { numbers: [10,20,30,40] }
+    return {}
   },
 
   render: function() {
     return (
-      <div>
-        <h1>My React Boilerplate</h1>
-        {this.state.numbers.map(function(elem, index) {
-          return <TweetBox key={index} maxTweetLength={50} />
-        })}
+      <div className="container">
+
+        <div className="display-box">
+          <h1>Current Chord:</h1>
+        </div>
+
+        <div className="settings-box">
+          {_.range(16).map(function(elem, index) {
+            return (
+              <div className="button settings-button" key={elem} id={"setting" + elem}>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="mem-scale-box">
+          {_.range(6).map(function(elem, index) {
+            return (
+              <div className="button chord-mem-button" key={elem} id={"mem" + elem}>
+              </div>
+            );
+          })}
+          {_.range(18).map(function(elem, index) {
+            return (
+              <div className="button scale-button" key={elem} id={"scale" + elem}>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="chord-options-box">
+          {_.range(8).map(function(elem, index) {
+            return (
+              <div className="button chord-options-button" key={elem} id={"option" + elem}>
+              </div>
+            );
+          })}
+
+        </div>
+
+        <div className="chord-matrix-box">
+          {_.range(32).map(function(elem, index) {
+            return (
+              <div className="button chord-matrix-button" key={elem} id={"pad" + elem}>
+              </div>
+            );
+          })}
+        </div>
+
+
       </div>
+
+
+
     )
   }
-
-});
+})
 
 module.exports = App;
